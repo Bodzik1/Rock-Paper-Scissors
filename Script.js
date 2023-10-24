4d5f88be932713164dbd0e0ba6a1ec74c8f00307
Bogdan Mieldzicz <66859022+Bodzik1@users.noreply.github.com>
2023-10-24 13:53:08 -0400

    added a feature where you can play 5 times and make youre own choice. Then checks who had the higher score and prints it out

--- Script.js
+++ Script.js
@@ -1,5 +1,41 @@
 function getComputerChoice(){
-    const rpc = ["Rock", "Paper", "Scissors"];
+    const rps = ["Rock", "Paper", "Scissors"];
     const num = Math.floor(Math.random() * 3);
-    return rpc[num];
+    return rps[num];
+} 
+
+function game(playerSelection, computerSelection){
+    let results;
+    let playerScore = 0;
+    let computerScore = 0;
+    let counter = 0;
+    while(counter < 5){
+        if(playerSelection == computerSelection){
+            playerScore += null;
+            computerScore += null;
+        }else if(computerSelection == "Paper"){
+            computerScore++;
+        }else{
+            playerScore++;
+        }
+        counter++;
+    }
+    
+    if(playerScore > computerScore){
+        results = "Player score " + playerScore + " computer score " + computerScore; 
+    }
+    else{
+        results = "Computer score " + computerScore + " player score " + playerScore ;
+    }
+
+
+    return results;
 }
+
+for (let i = 0; i < 5; i++) {
+    playerChoice = prompt("Enter youre choice");
+}
+
+let playerSelection = playerChoice;
+const computerSelection = getComputerChoice();
+console.log(game(playerSelection, computerSelection));
