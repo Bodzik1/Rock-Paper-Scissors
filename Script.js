cc846fb09cef82cd6a80aa0affdcd1b8ba0d714f
Bogdan Mieldzicz <66859022+Bodzik1@users.noreply.github.com>
2023-10-25 21:00:08 -0400

    started to implement button choices in javascript

--- Script.js
+++ Script.js
@@ -1,41 +1,13 @@
-function getComputerChoice(){
-    const rps = ["Rock", "Paper", "Scissors"];
-    const num = Math.floor(Math.random() * 3);
-    return rps[num];
-} 
+const buttons = document.querySelectorAll('#buttons');
 
-function game(playerSelection, computerSelection){
-    let results;
-    let playerScore = 0;
-    let computerScore = 0;
-    let counter = 0;
-    while(counter < 5){
-        if(playerSelection == computerSelection){
-            playerScore += null;
-            computerScore += null;
-        }else if(computerSelection == "Paper"){
-            computerScore++;
-        }else{
-            playerScore++;
-        }
-        counter++;
-    }
-    
-    if(playerScore > computerScore){
-        results = "Player score " + playerScore + " computer score " + computerScore; 
-    }
-    else{
-        results = "Computer score " + computerScore + " player score " + playerScore ;
-    }
+// Add click event listeners to each button
+buttons.forEach(button => {
+  button.addEventListener("click", function(event) {
+    buttonClickHandler(event.target.textContent);
+  });
+});
 
-
-    return results;
+// Function to handle button click
+function buttonClickHandler(buttonText) {
+  alert(buttonText + " was clicked!");
 }
-
-for (let i = 0; i < 5; i++) {
-    playerChoice = prompt("Enter youre choice");
-}
-
-let playerSelection = playerChoice;
-const computerSelection = getComputerChoice();
-console.log(game(playerSelection, computerSelection));
