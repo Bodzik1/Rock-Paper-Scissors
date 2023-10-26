5c042a9de704ed3224cd2f02ba103396a5f9fc05
Bogdan Mieldzicz <66859022+Bodzik1@users.noreply.github.com>
2023-10-26 00:54:16 -0400

    added 95% of functionality

--- Script.js
+++ Script.js
@@ -1,13 +1,30 @@
-const buttons = document.querySelectorAll('#buttons');
+const choices = ["rock", "paper", "scissors"];
 
-// Add click event listeners to each button
-buttons.forEach(button => {
-  button.addEventListener("click", function(event) {
-    buttonClickHandler(event.target.textContent);
-  });
-});
-
-// Function to handle button click
-function buttonClickHandler(buttonText) {
-  alert(buttonText + " was clicked!");
+function computerPlay() {
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function game(playerChoice) {
+    const computerChoice = computerPlay();
+    const result = playRound(playerChoice, computerChoice);
+    document.getElementById("result").textContent = result;
+}
+
+  document.getElementById("rock").addEventListener("click", () => game("rock"));
+  document.getElementById("paper").addEventListener("click", () => game("paper"));
+  document.getElementById("scissors").addEventListener("click", () => game("scissors"));
